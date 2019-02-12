999f1671936eace7423f72c44350f45e8c134b42
aroraakshit <akshit.arora@colorado.edu>
2019-02-11 20:24:04 -0700

    partition k equal sum subset

---
+++ partition_k_equal_sum_subset.py
@@ -0,0 +1,45 @@
+# Credits - LeetCode #TODO
+
+# Approach 1: Search by Constructing Subset sums O(k^(N−k) k!), where N is the length of nums, and k is as given
+class Solution(object):
+    def canPartitionKSubsets(self, nums, k):
+        target, rem = divmod(sum(nums), k)
+        if rem: return False
+
+        def search(groups):
+            if not nums: return True
+            v = nums.pop()
+            for i, group in enumerate(groups):
+                if group + v <= target:
+                    groups[i] += v
+                    if search(groups): return True
+                    groups[i] -= v
+                if not group: break
+            nums.append(v)
+            return False
+
+        nums.sort()
+        if nums[-1] > target: return False
+        while nums and nums[-1] == target:
+            nums.pop()
+            k -= 1
+
+        return search([0] * k)
+
+# Approach 2: Dynamic Programming on Subsets of Input 
+class Solution(object):
+    def canPartitionKSubsets(self, nums, k):
+        target, rem = divmod(sum(nums), k)
+        if rem or max(nums) > target: return False
+
+        memo = [None] * (1 << len(nums))
+        memo[-1] = True
+        def search(used, todo):
+            if memo[used] is None:
+                targ = (todo - 1) % target + 1
+                memo[used] = any(search(used | (1<<i), todo - num)
+                                 for i, num in enumerate(nums)
+                                 if (used >> i) & 1 == 0 and num <= targ)
+            return memo[used]
+
+        return search(0, target * k)
