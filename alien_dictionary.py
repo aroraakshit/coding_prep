c368021e38ff38c663b1fdd51b05d5d2cf60291b
aroraakshit <akshit.arora@colorado.edu>
2019-07-25 07:13:51 -0700

    alien dictionary

---
+++ alien_dictionary.py
@@ -0,0 +1,19 @@
+class Solution(object): # Python2 solution, Credits - LeetCode, 20ms, faster than 80% solutions
+    #   Topological sort
+    def alienOrder(self, words):
+        less = []
+        for pair in zip(words, words[1:]):
+            for a, b in zip(*pair):
+                if a != b:
+                    less += a + b,
+                    break
+        chars = set(''.join(words))
+        order = []
+        while less:
+            free = chars - set(zip(*less)[1])
+            if not free:
+                return ''
+            order += free
+            less = filter(free.isdisjoint, less)
+            chars -= free
+        return ''.join(order + list(chars))
